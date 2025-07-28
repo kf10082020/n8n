7f96cb3abfb534d830f54c8eccf446a6ab49b9ef
kf10082020 <kf10082020@gmail.com>
2025-07-28 16:43:19 +0300

    Update workflowsPage.ts

--- cypress/composables/workflowsPage.ts
+++ cypress/composables/workflowsPage.ts
@@ -1,19 +1,39 @@
 /**
- * Getters
+ * Workflows Page Composable
  */
 
-export function getWorkflowsPageUrl() {
-	return '/home/workflows';
-}
+//////////////////////////////
+// URL / Navigation
+//////////////////////////////
+
+export const getWorkflowsPageUrl = (): string => '/home/workflows';
+
+export const visitWorkflowsPage = (): void => {
+	cy.visit(getWorkflowsPageUrl());
+	cy.get('h1').contains(/workflows/i).should('exist');
+};
+
+//////////////////////////////
+// UI Getters
+//////////////////////////////
 
 export const getCreateWorkflowButton = () => cy.getByTestId('add-resource-workflow');
 
 export const getNewWorkflowCardButton = () => cy.getByTestId('new-workflow-card');
 
-/**
- * Actions
- */
+export const getWorkflowCardsList = () => cy.getByTestId('workflow-card');
 
-export function visitWorkflowsPage() {
-	cy.visit(getWorkflowsPageUrl());
-}
+export const getSearchInput = () => cy.get('input[placeholder="Search"]');
+
+//////////////////////////////
+// Actions
+//////////////////////////////
+
+export const createNewWorkflow = () => {
+	getCreateWorkflowButton().click();
+	getNewWorkflowCardButton().click();
+};
+
+export const searchWorkflowByName = (name: string) => {
+	getSearchInput().clear().type(name);
+};
