a03e727619a157d88a3d162f56fec356c70a7686
kf10082020 <kf10082020@gmail.com>
2025-07-28 21:44:56 +0300

    Update index.ts

--- cypress/utils/index.ts
+++ cypress/utils/index.ts
@@ -1,3 +1,29 @@
+// ✅ Базовые утилиты
 export * from './executions';
 export * from './modal';
 export * from './popper';
+
+// ✅ Работа с локаторами, элементами и DOM
+export * from './selectors';
+export * from './interactions';
+export * from './workflow';
+export * from './nodes';
+
+// ✅ Работа с API и HTTP-запросами
+export * from './requests';
+export * from './auth';
+export * from './cookies';
+
+// ✅ Генерация данных, фейковые пользователи, идентификаторы
+export * from './data';
+export * from './uuid';
+export * from './faker';
+
+// ✅ Таймеры, отложенные действия, отладка
+export * from './timing';
+export * from './logger';
+export * from './debug';
+
+// ✅ Навигация и роутинг
+export * from './navigation';
+export * from './routing';
