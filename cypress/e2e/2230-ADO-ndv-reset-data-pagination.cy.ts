b216c9ea3ad7e230a650ad4dc5219f545a5db923
kf10082020 <kf10082020@gmail.com>
2025-07-28 17:37:52 +0300

    Update 2230-ADO-ndv-reset-data-pagination.cy.ts

--- cypress/e2e/2230-ADO-ndv-reset-data-pagination.cy.ts
+++ cypress/e2e/2230-ADO-ndv-reset-data-pagination.cy.ts
@@ -5,29 +5,31 @@ const workflowPage = new WorkflowPage();
 const ndv = new NDV();
 
 describe('ADO-2230 NDV Pagination Reset', () => {
-	it('should reset pagaintion if data size changes to less than current page', () => {
-		// setup, load workflow with debughelper node with random seed
+	it('should reset pagination if data size changes to less than current page', () => {
+		cy.log('📦 Visit workflow editor');
 		workflowPage.actions.visit();
+
+		cy.log('📂 Load fixture workflow with DebugHelper node');
 		cy.createFixtureWorkflow('NDV-debug-generate-data.json', 'Debug workflow');
 		workflowPage.actions.openNode('DebugHelper');
 
-		// execute node outputting 10 pages, check output of first page
+		cy.log('🚀 Execute node outputting 10 pages and check first page');
 		ndv.actions.execute();
 		clearNotifications();
 		ndv.getters.outputTbodyCell(1, 1).invoke('text').should('eq', 'Terry.Dach@hotmail.com');
 
-		// open 4th page, check output
+		cy.log('📃 Open 4th page and check output');
 		ndv.getters.pagination().should('be.visible');
 		ndv.getters.pagination().find('li.number').should('have.length', 5);
 		ndv.getters.pagination().find('li.number').eq(3).click();
 		ndv.getters.outputTbodyCell(1, 1).invoke('text').should('eq', 'Shane.Cormier68@yahoo.com');
 
-		// output a lot less data
+		cy.log('🔄 Change data amount to 20 and re-execute');
 		ndv.getters.parameterInput('randomDataCount').find('input').clear().type('20');
 		ndv.actions.execute();
 		clearNotifications();
 
-		// check we are back to second page now
+		cy.log('✅ Assert pagination resets to page 1 with new data');
 		ndv.getters.pagination().find('li.number').should('have.length', 2);
 		ndv.getters.outputTbodyCell(1, 1).invoke('text').should('eq', 'Sylvia.Weber@hotmail.com');
 	});
