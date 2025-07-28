8a4b3659222c0ac8f46350349ce669b966419d75
kf10082020 <kf10082020@gmail.com>
2025-07-28 17:29:40 +0300

    Update 1338-ADO-ndv-missing-input-panel.cy.ts

--- cypress/e2e/1338-ADO-ndv-missing-input-panel.cy.ts
+++ cypress/e2e/1338-ADO-ndv-missing-input-panel.cy.ts
@@ -4,21 +4,21 @@ import { successToast } from '../pages/notifications';
 const workflowPage = new WorkflowPageClass();
 const ndv = new NDV();
 
-describe('ADO-1338-ndv-missing-input-panel', () => {
+describe('ADO-1338: NDV missing input/output panel regression', () => {
 	beforeEach(() => {
 		workflowPage.actions.visit();
 	});
 
-	it('should show the input and output panels when node is missing input and output data', () => {
+	it('should show both input and output panels when node has no runtime data', () => {
 		cy.createFixtureWorkflow('Test_ado_1338.json');
 
-		// Execute the workflow
 		workflowPage.getters.zoomToFitButton().click();
 		workflowPage.getters.executeWorkflowButton().click();
-		// Check success toast (works because Cypress waits enough for the element to show after the http request node has finished)
+
 		successToast().should('be.visible');
 
 		workflowPage.actions.openNode('Discourse1');
+
 		ndv.getters.inputPanel().should('be.visible');
 		ndv.getters.outputPanel().should('be.visible');
 	});
