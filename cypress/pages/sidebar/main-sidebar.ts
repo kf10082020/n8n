6414f642a6b628a262028cb2cf2d0fbbecb5d4a2
kf10082020 <kf10082020@gmail.com>
2025-07-28 22:48:16 +0300

    Update main-sidebar.ts

--- cypress/pages/sidebar/main-sidebar.ts
+++ cypress/pages/sidebar/main-sidebar.ts
@@ -1,50 +1,60 @@
-import { BasePage } from '../base';
-import { WorkflowsPage } from '../workflows';
+// cypress/composables/sidebar/index.ts
+// Единый модуль взаимодействия с боковым меню (main + settings)
 
 /**
- * @deprecated Use functional composables from @composables instead.
- * If a composable doesn't exist for your use case, please create a new one in:
- * cypress/composables
- *
- * This class-based approach is being phased out in favor of more modular functional composables.
- * Each getter and action in this class should be moved to individual composable functions.
+ * ✅ Геттеры элементов бокового меню
  */
-export class MainSidebar extends BasePage {
-	getters = {
-		menuItem: (id: string) => cy.getByTestId('menu-item').get('#' + id),
-		settings: () => this.getters.menuItem('settings'),
-		settingsBack: () => cy.getByTestId('settings-back'),
-		templates: () => this.getters.menuItem('templates'),
-		workflows: () => this.getters.menuItem('workflows'),
-		credentials: () => this.getters.menuItem('credentials'),
-		executions: () => this.getters.menuItem('executions'),
-		adminPanel: () => this.getters.menuItem('cloud-admin'),
-		userMenu: () => cy.getByTestId('user-menu'),
-		logo: () => cy.getByTestId('n8n-logo'),
-	};
 
-	actions = {
-		goToSettings: () => {
-			this.getters.userMenu().click();
-			cy.getByTestId('user-menu-item-settings').should('be.visible').click();
-		},
-		goToCredentials: () => {
-			this.getters.credentials().should('be.visible');
-			cy.get('[data-old-overflow]').should('not.exist');
-			this.getters.credentials().click();
-		},
-		openUserMenu: () => {
-			this.getters.userMenu().click();
-		},
-		closeSettings: () => {
-			this.getters.settingsBack().click();
-		},
-		signout: () => {
-			const workflowsPage = new WorkflowsPage();
-			cy.visit(workflowsPage.url);
-			this.actions.openUserMenu();
-			cy.getByTestId('user-menu-item-logout').click();
-			cy.wrap(Cypress.session.clearAllSavedSessions());
-		},
-	};
-}
+export const getSidebarMenuItem = (id: string) => cy.getByTestId('menu-item').get(`#${id}`);
+
+export const getSidebarLogo = () => cy.getByTestId('n8n-logo');
+export const getUserMenu = () => cy.getByTestId('user-menu');
+export const getSettingsMenuItem = () => getSidebarMenuItem('settings');
+export const getTemplatesMenuItem = () => getSidebarMenuItem('templates');
+export const getWorkflowsMenuItem = () => getSidebarMenuItem('workflows');
+export const getCredentialsMenuItem = () => getSidebarMenuItem('credentials');
+export const getExecutionsMenuItem = () => getSidebarMenuItem('executions');
+export const getAdminPanelMenuItem = () => getSidebarMenuItem('cloud-admin');
+export const getSettingsBackButton = () => cy.getByTestId('settings-back');
+
+/**
+ * ✅ Геттеры элементов сайдбара настроек (settings sidebar)
+ */
+
+export const getSettingsSidebar = () => cy.getByTestId('settings-sidebar');
+export const getSettingsSidebarTitle = () => cy.getByTestId('settings-sidebar-title');
+export const getSettingsSidebarItem = (id: string) => cy.get(`#${id}`);
+
+/**
+ * ✅ Действия с сайдбаром
+ */
+
+export const openUserMenu = () => {
+	getUserMenu().click();
+};
+
+export const goToSettings = () => {
+	openUserMenu();
+	cy.getByTestId('user-menu-item-settings').should('be.visible').click();
+};
+
+export const goToCredentials = () => {
+	getCredentialsMenuItem().should('be.visible');
+	cy.get('[data-old-overflow]').should('not.exist');
+	getCredentialsMenuItem().click();
+};
+
+export const goToWorkflows = () => {
+	getWorkflowsMenuItem().click();
+};
+
+export const closeSettingsSidebar = () => {
+	getSettingsBackButton().click();
+};
+
+export const signout = () => {
+	cy.visit('/workflows');
+	openUserMenu();
+	cy.getByTestId('user-menu-item-logout').click();
+	cy.wrap(Cypress.session.clearAllSavedSessions());
+};
