6a8f7a9db5009d0fecb97ab79e5775b1f33744cc
kf10082020 <kf10082020@gmail.com>
2025-07-28 21:47:04 +0300

    Update popper.ts

--- cypress/utils/popper.ts
+++ cypress/utils/popper.ts
@@ -1,15 +1,34 @@
-export function getPopper() {
+/**
+ * Получает все popper-элементы (например, выпадающие списки, тултипы и селекты).
+ */
+export function getPopper(): Cypress.Chainable<JQuery<HTMLElement>> {
 	return cy.get('.el-popper');
 }
 
-export function getVisiblePopper() {
+/**
+ * Возвращает только видимые popper-элементы (с aria-hidden="false").
+ */
+export function getVisiblePopper(): Cypress.Chainable<JQuery<HTMLElement>> {
 	return getPopper().filter('[aria-hidden="false"]');
 }
 
-export function getVisibleSelect() {
+/**
+ * Возвращает видимый элемент popper-селекта (элемент el-select__popper).
+ */
+export function getVisibleSelect(): Cypress.Chainable<JQuery<HTMLElement>> {
 	return getVisiblePopper().filter('.el-select__popper');
 }
 
-export function getVisibleDropdown() {
+/**
+ * Возвращает видимый элемент popper-дропдауна (элемент el-dropdown__popper).
+ */
+export function getVisibleDropdown(): Cypress.Chainable<JQuery<HTMLElement>> {
 	return getVisiblePopper().filter('.el-dropdown__popper');
+}
+
+/**
+ * Возвращает видимый tooltip popper (например, всплывающие подсказки).
+ */
+export function getVisibleTooltip(): Cypress.Chainable<JQuery<HTMLElement>> {
+	return getVisiblePopper().filter('.el-tooltip__popper');
 }
