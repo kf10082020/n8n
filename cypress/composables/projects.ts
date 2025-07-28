e42936e3aa0a151bda7f5e202388f239e0f04905
kf10082020 <kf10082020@gmail.com>
2025-07-28 16:55:09 +0300

    Update projects.ts

--- cypress/composables/projects.ts
+++ cypress/composables/projects.ts
@@ -4,44 +4,58 @@ import { getVisibleSelect } from '../utils';
 const workflowPage = new WorkflowPage();
 const credentialsModal = new CredentialsModal();
 
+/**
+ * Navigation & Project menu
+ */
 export const getHomeButton = () => cy.getByTestId('project-home-menu-item');
 export const getPersonalProjectsButton = () => cy.getByTestId('project-personal-menu-item');
 export const getMenuItems = () => cy.getByTestId('project-menu-item');
+
+/**
+ * Add project button in main nav
+ */
 export const getAddProjectButton = () => {
 	cy.getByTestId('universal-add').should('be.visible').click();
+
 	cy.getByTestId('universal-add')
 		.find('.el-sub-menu__title')
-		.as('menuitem')
-		.should('have.attr', 'aria-describedby');
-
-	cy.get('@menuitem')
 		.invoke('attr', 'aria-describedby')
-		.then((el) => cy.get(`[id="${el}"]`))
-		.as('submenu');
+		.then((id) => {
+			cy.get(`#${id}`)
+				.findByTestId('navigation-menu-item')
+				.filter(':contains("Project")')
+				.as('projectAddButton');
+		});
 
-	cy.get('@submenu').within((submenu) =>
-		cy
-			.wrap(submenu)
-			.getByTestId('navigation-menu-item')
-			.should('be.visible')
-			.filter(':contains("Project")')
-			.as('button'),
-	);
-
-	return cy.get('@button');
+	return cy.get('@projectAddButton');
 };
+
+/**
+ * Add project button when no projects exist
+ */
 export const getAddFirstProjectButton = () => cy.getByTestId('add-first-project-button');
+
+/**
+ * Project Icon Picker
+ */
 export const getIconPickerButton = () => cy.getByTestId('icon-picker-button');
-export const getIconPickerTab = (tab: string) => cy.getByTestId('icon-picker-tabs').contains(tab);
+export const getIconPickerTab = (tab: string) =>
+	cy.getByTestId('icon-picker-tabs').contains(tab);
 export const getIconPickerIcons = () => cy.getByTestId('icon-picker-icon');
 export const getIconPickerEmojis = () => cy.getByTestId('icon-picker-emoji');
-// export const getAddProjectButton = () =>
-// 	cy.getByTestId('universal-add').should('contain', 'Add project').should('be.visible');
+
+/**
+ * Project Tabs
+ */
 export const getProjectTabs = () => cy.getByTestId('project-tabs').find('a');
 export const getProjectTabWorkflows = () => getProjectTabs().filter('a[href$="/workflows"]');
 export const getProjectTabCredentials = () => getProjectTabs().filter('a[href$="/credentials"]');
 export const getProjectTabExecutions = () => getProjectTabs().filter('a[href$="/executions"]');
 export const getProjectTabSettings = () => getProjectTabs().filter('a[href$="/settings"]');
+
+/**
+ * Project Settings
+ */
 export const getProjectSettingsNameInput = () =>
 	cy.getByTestId('project-settings-name-input').find('input');
 export const getProjectSettingsSaveButton = () => cy.getByTestId('project-settings-save-button');
@@ -49,30 +63,56 @@ export const getProjectSettingsCancelButton = () =>
 	cy.getByTestId('project-settings-cancel-button');
 export const getProjectSettingsDeleteButton = () =>
 	cy.getByTestId('project-settings-delete-button');
+
+/**
+ * Project Members
+ */
 export const getProjectMembersSelect = () => cy.getByTestId('project-members-select');
+
+/**
+ * Add member to project with optional role
+ * @param email - Email of the user to add
+ * @param role - Optional role (e.g., Editor, Viewer)
+ */
 export const addProjectMember = (email: string, role?: string) => {
 	getProjectMembersSelect().click();
-	getProjectMembersSelect().get('.el-select-dropdown__item').contains(email.toLowerCase()).click();
+	getProjectMembersSelect()
+		.get('.el-select-dropdown__item')
+		.contains(email.toLowerCase())
+		.click();
 
 	if (role) {
 		cy.getByTestId(`user-list-item-${email}`)
-			.find('[data-test-id="projects-settings-user-role-select"]')
+			.findByTestId('projects-settings-user-role-select')
 			.click();
+
 		getVisibleSelect().find('li').contains(role).click();
 	}
 };
+
+/**
+ * Resource/Folder Move Modals
+ */
 export const getResourceMoveModal = () => cy.getByTestId('project-move-resource-modal');
 export const getProjectMoveSelect = () => cy.getByTestId('project-move-resource-modal-select');
 export const getProjectSharingSelect = () => cy.getByTestId('project-sharing-select');
 export const getMoveToFolderSelect = () => cy.getByTestId('move-to-folder-dropdown');
 
+/**
+ * Create new project via UI
+ * @param name - Name of the new project
+ */
 export function createProject(name: string) {
 	getAddProjectButton().click();
-
 	getProjectSettingsNameInput().should('be.visible').clear().type(name);
 	getProjectSettingsSaveButton().click();
 }
 
+/**
+ * Create workflow from fixture file
+ * @param fixtureKey - Filename in fixtures folder
+ * @param name - Desired workflow name
+ */
 export function createWorkflow(fixtureKey: string, name: string) {
 	workflowPage.getters.workflowImportInput().selectFile(`fixtures/${fixtureKey}`, { force: true });
 	workflowPage.actions.setWorkflowName(name);
@@ -80,6 +120,11 @@ export function createWorkflow(fixtureKey: string, name: string) {
 	workflowPage.actions.zoomToFit();
 }
 
+/**
+ * Create a Notion credential
+ * @param name - Credential name
+ * @param closeModal - Whether to close the modal after creation
+ */
 export function createCredential(name: string, closeModal = true) {
 	credentialsModal.getters.newCredentialModal().should('be.visible');
 	credentialsModal.getters.newCredentialTypeSelect().should('be.visible');
