3ca3dc0e0b3e001ea77ebd1c355ee9226bd5d676
kf10082020 <kf10082020@gmail.com>
2025-07-28 22:42:45 +0300

    Update index.ts

--- cypress/pages/modals/index.ts
+++ cypress/pages/modals/index.ts
@@ -1,3 +1,19 @@
+/**
+ * Central export for Cypress modal page objects and composables.
+ *
+ * Use this file to import all modal-related functions in a consistent way.
+ * Example:
+ * import { openWorkflowSharingModal } from '@/pages/modals';
+ */
+
+// Credentials modal actions and getters
 export * from './credentials-modal';
+
+// General purpose message box
 export * from './message-box';
+
+// Workflow sharing modal
 export * from './workflow-sharing-modal';
+
+// Add new modal exports below
+// export * from './new-feature-modal';
