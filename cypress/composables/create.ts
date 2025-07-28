9907ba2aa7ecb4b211755b60d9075107d7b108ba
kf10082020 <kf10082020@gmail.com>
2025-07-28 16:39:19 +0300

    Update create.ts

--- cypress/composables/create.ts
+++ cypress/composables/create.ts
@@ -1,19 +1,40 @@
-export const universalAddButton = () => cy.getByTestId('universal-add');
+/**
+ * ====================================
+ * Universal Resource Creation Composable
+ * ====================================
+ * Унифицированный обработчик создания workflow / проекта / credential.
+ */
 
+/**
+ * Возвращает универсальную кнопку "Add".
+ */
+export const universalAddButton = () => {
+	return cy.getByTestId('universal-add').should('be.visible');
+};
+
+/**
+ * Создаёт новый ресурс указанного типа.
+ *
+ * @param resourceType Тип ресурса: 'project', 'workflow', 'credential'
+ * @param projectName Имя проекта, если создаётся workflow/credential
+ */
 export const createResource = (
 	resourceType: 'project' | 'workflow' | 'credential',
 	projectName: string,
 ) => {
 	universalAddButton().click();
+
+	// Выбор типа ресурса
 	cy.getByTestId('navigation-submenu')
-		.contains(new RegExp(resourceType, 'i'))
 		.should('be.visible')
+		.contains(new RegExp(`^${resourceType}$`, 'i')) // точное соответствие
 		.click();
 
+	// Привязка к проекту (если не создаётся сам проект)
 	if (resourceType !== 'project') {
 		cy.getByTestId('navigation-submenu-item')
-			.contains(new RegExp(projectName))
 			.should('be.visible')
+			.contains(new RegExp(projectName, 'i'))
 			.click();
 	}
 };
